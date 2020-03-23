7719a8ad2df37b29ec6cd567537f59edf9feb8eb
memolog <proone2013@gmail.com>
2020-03-23 13:11:17 +0000

    Scripts removed from master

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-git clone https://github.com/Artemmkin/reddit.git
-cd reddit
-gem install bundler:1.15.3
-bundle install
-puma -d
-ps aux | grep puma
--- setupmongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo rm /etc/apt/sources.list.d/mongodb*.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-sudo systemctl status mongod
